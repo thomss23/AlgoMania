4011f8f9266c0d8a586ba5d2ff677d4324e2af96
Thomas Borbeli <borbelithomas@gmail.com>
2021-06-17 10:18:07 +0300

    Add AP

---
+++ src/Graph/ArticulationPointTarjan.java
@@ -0,0 +1,22 @@
+package Graph;
+
+/* 
+A node is AP if by removing it, the no. of components increases
+
+Conditions:
+
+1) If node u is root of dfs tree and has at least 2 children(subgraphs)
+
+2)Node u is not root of DFS tree and it has a child V such that no vertex
+in subtree rooted with V has a backedge to one of the ancestors of u
+
+Logic :
+
+1) 
+
+
+*/
+public class ArticulationPointTarjan {
+
+    
+}
